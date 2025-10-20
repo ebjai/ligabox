b20635767674f01d974ae29a1c4c12592462ce1b
ebjai <eddiebarberiz@gmail.com>
2025-10-19 20:45:46 -0400

    Create App.tsx

---
+++ ligabox-final/src/App.tsx
@@ -0,0 +1,9 @@
+import Home2035 from "./pages/Home2035";
+
+export default function App() {
+  return (
+    <main className="min-h-screen bg-black text-[#f1d6a5]">
+      <Home2035 />
+    </main>
+  );
+}
